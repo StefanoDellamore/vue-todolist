6cb2d5c33696f0248d538510004834684e298ab8
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-09 19:03:04 +0100

    Finalizzazione esercizio

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,25 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Template HTML CSS Vue',
+            todos: [
+                { text: 'Giocare a Europa Universalis IV', done: false },
+                { text: 'Studiare Vue.js', done: true },
+                ],                
+                newTodoText: '',
         };
-    }
+    },
+    methods: {
+      removeTodo(index) {
+        this.todos.splice(index, 1);
+      },
 
-}).mount('#app');
+      addTodo() {
+        if (this.newTodoText.trim() !== '') {
+          this.todos.push({ text: this.newTodoText, done: false });
+          this.newTodoText = '';
+        }
+      },
+    },
+
+  }).mount('#app');
+  
